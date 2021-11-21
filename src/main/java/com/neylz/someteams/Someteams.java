658d2aca68175a06827b4e2b7122992247dcb0bf
Neylz <neylzvii@gmail.com>
2021-11-22 00:09:18 +0100

    updating to com.neylz

---
+++ src/main/java/com/neylz/someteams/Someteams.java
@@ -0,0 +1,11 @@
+package com.neylz.someteams;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Someteams implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
